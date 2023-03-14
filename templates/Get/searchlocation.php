e4fe5f60e8e3a000737c6857db09094a2b788ffb
Fábio Freitas <fabiofreitassilvacontato@gmail.com>
2023-03-14 09:12:26 -0300

    Atualização do SearchLocation

---
+++ templates/Get/searchlocation.php
@@ -0,0 +1,12 @@
+<?php
+
+$listLocations = array();
+foreach ($locations as $locationsSingle) {
+    $listLocations['results'][] = array(
+        'id' => $locationsSingle['id'],
+        'text' => $locationsSingle['address'],
+    );
+}
+echo json_encode($listLocations);
+
+?>
